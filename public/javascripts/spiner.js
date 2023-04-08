7fdf1bda2bd45429681435fa6d1abd4bb199b9e0
Juan <juanchipicabohm2992@gmail.com>
2023-04-08 17:28:30 -0300

    Se acomodo la vista de propiedades, las de alquiler, venta y alquiler temporario. Hay un ejemplo de preload

---
+++ public/javascripts/spiner.js
@@ -0,0 +1,11 @@
+/*window.addEventListener('load', () => {
+    const spinner = document.querySelector('.loader');
+    spinner.style.display = 'none';
+});*/
+
+window.addEventListener('load', () => {
+    const spinner = document.querySelector('.loader');
+    const overlay = document.querySelector('.overlay');
+    spinner.style.display = 'none';
+    overlay.style.display = 'none';
+  });
